165c214a8d70ec4aa7315becc7c0e921ec6043fc
Nadir Urbina <nurbinabr@gmail.com>
2025-03-21 22:05:57 -0400

    fixing previous deployment errors

--- sanity/schemas/lessonProgress.ts
+++ sanity/schemas/lessonProgress.ts
@@ -1,3 +1,5 @@
+import { Rule } from '@sanity/types'
+
 export default {
   name: 'lessonProgress',
   title: 'Lesson Progress',
@@ -8,21 +10,21 @@ export default {
       title: 'User',
       type: 'reference',
       to: [{ type: 'userProfile' }],
-      validation: Rule => Rule.required()
+      validation: (Rule: Rule) => Rule.required()
     },
     {
       name: 'lesson',
       title: 'Lesson',
       type: 'reference',
       to: [{ type: 'lesson' }],
-      validation: Rule => Rule.required()
+      validation: (Rule: Rule) => Rule.required()
     },
     {
       name: 'course',
       title: 'Course',
       type: 'reference',
       to: [{ type: 'course' }],
-      validation: Rule => Rule.required(),
+      validation: (Rule: Rule) => Rule.required(),
       description: 'The course this lesson belongs to (for easier querying)'
     },
     {
@@ -35,7 +37,7 @@ export default {
       name: 'watchedPercentage',
       title: 'Watched Percentage',
       type: 'number',
-      validation: Rule => Rule.min(0).max(100),
+      validation: (Rule: Rule) => Rule.min(0).max(100),
       initialValue: 0
     },
     {
